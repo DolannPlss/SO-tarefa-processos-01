88c19a8feb8836bbb7e5336b36f89e749a9d7049
marco <marco@DESKTOP-KN07FDS>
2020-09-06 18:41:55 -0300

    Primeiro Commit

---
+++ SO1 Tarefa Processos/src/controller/RedesController.java
@@ -0,0 +1,127 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+
+public class RedesController {
+	public String os() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
+	
+	public void ipConfig(String os) {
+		if (os.contains("Windows")) {
+			StringBuffer buffer2= new StringBuffer();
+				try {
+					boolean ipv4 = false;
+					Process p = Runtime.getRuntime().exec("ipconfig");
+					InputStream fluxo = p.getInputStream();
+					InputStreamReader leitor = new InputStreamReader(fluxo);
+					BufferedReader buffer = new BufferedReader(leitor);
+					String linha = buffer.readLine();
+					buffer2.append(linha);
+					buffer2.append("\n");
+					while (linha != null) {
+						if (linha.contains("IPv4")) {
+							ipv4 = true;
+						}
+							if (linha.contains("Gateway")) {
+								if (ipv4 == true) {
+										System.out.println(buffer2);
+									ipv4 = false;
+									buffer2.delete(0, buffer2.length());
+								}
+							}					
+						linha = buffer.readLine();
+						buffer2.append(linha);
+						buffer2.append("\n");
+					}
+					buffer.close();
+					leitor.close();
+					fluxo.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+			}
+		}else{
+			StringBuffer buffer2= new StringBuffer();
+			try {
+				boolean ipv4 = false;
+				Process p = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				buffer2.append(linha);
+				buffer2.append("\n");
+				while (linha != null) {
+					if (linha.contains("IPv4")) {
+						ipv4 = true;
+					}
+						if (linha.contains("Gateway")) {
+							if (ipv4 == true) {
+									System.out.println(buffer2);
+								ipv4 = false;
+								buffer2.delete(0, buffer2.length());
+							}
+						}					
+					linha = buffer.readLine();
+					buffer2.append(linha);
+					buffer2.append("\n");
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+		}
+		}
+	}
+	public void pingTest ( String os){
+		if(os.contains("Windows")) {
+			try {
+				Process p = Runtime.getRuntime().exec("ping -n 10 www.google.com.br");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("M‚dia")) {
+						
+						System.out.println("média do ping= "+linha.substring(40,45));
+						}
+						linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			try {
+				Process p = Runtime.getRuntime().exec("ping -n 10 www.google.com.br");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("M‚dia")) {
+						System.out.println("média= "+linha.substring(40,45));
+						}
+						linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ SO1 Tarefa Processos/src/view/Main.java
@@ -0,0 +1,33 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.RedesController;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		RedesController redController = new RedesController();
+		
+		String os = redController.os();
+		System.out.println(os);
+		int opc = 0;
+		while (opc!=9){
+			opc=Integer.parseInt(JOptionPane.showInputDialog("insira comando\n 1-ipconfig\n 2-ping test	\n 9-finalizar\n "));
+			switch (opc) {
+			
+			case 1:
+				redController.ipConfig(os);
+				break;
+			case 2:
+				redController.pingTest(os);
+				break;
+			default: System.out.println("opc invalida");
+				break;
+			case 9:System.out.println("operação finalizada");
+				break;
+			}
+	
+		}
+	}
+}
