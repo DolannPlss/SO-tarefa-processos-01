3d5a921203e018e1f5367cfbd770eb29e741fc00
marco <marco@DESKTOP-KN07FDS>
2020-09-07 02:04:19 -0300

    finalizado

--- SO1 Tarefa Processos/src/view/Main.java
+++ SO1 Tarefa Processos/src/view/Main.java
@@ -10,7 +10,6 @@ public class Main {
 		RedesController redController = new RedesController();
 		
 		String os = redController.os();
-		System.out.println(os);
 		int opc = 0;
 		while (opc!=9){
 			opc=Integer.parseInt(JOptionPane.showInputDialog("insira comando\n 1-ipconfig\n 2-ping test	\n 9-finalizar\n "));
